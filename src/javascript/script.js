b60955940d1f07afb90d944b089d34fe2b6f275f
sifenbeshada <sefanrobi@gmail.com>
2023-12-22 23:18:37 +0300

    Used Tailwind css framework for the website

---
+++ src/javascript/script.js
@@ -0,0 +1,8 @@
+        const menuToggle = document.getElementById('menu-toggle');
+        const menuItems = document.getElementById('menu-items');
+    
+        menuToggle.addEventListener('click', function() {
+            menuItems.classList.toggle('hidden');
+        });
+    
+
